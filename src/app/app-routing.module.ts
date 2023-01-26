f64119972540b3f2840e772cde18066d232074f0
LAPTOP-NE0HBHC5\Laci <feherlaci99@gmail.com>
2023-01-26 23:52:45 +0100

    remove sass duplication + change components structure + add responsive layout design + start neumorphism design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TodoBoardComponent } from './component/todo-board/todo-board.component';
+import { HomeComponent } from './component/home/home.component';
 import { TodoDetailComponent } from './component/todo-detail/todo-detail.component';
 
 const routes: Routes = [
-  {path: '', component: TodoBoardComponent},
+  {path: '', component: HomeComponent},
   {path: 'todo/:id', component: TodoDetailComponent},
   {path: '**', redirectTo: "", pathMatch: 'full'}];
 
